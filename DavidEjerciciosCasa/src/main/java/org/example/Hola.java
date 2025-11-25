9a57f602d6e9b38ee42bdb6338ed6b4e87849908
DESKTOP-0EBKTRK\USUARIO <davidginer.alicante@gmail.com>
2025-11-25 20:34:54 +0100

    Práctica ZXSpectrum

---
+++ DavidEjerciciosCasa/src/main/java/org/example/Hola.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Hola {
+
+    static void main() {
+
+        String s = "Yava".replace("Y", "imbecil");
+        System.out.println(s);
+
+
+
+    }
+
+
+}
